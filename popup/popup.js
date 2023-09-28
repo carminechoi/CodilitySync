634cc236fac8232eb035c28d4e18e3eb04a343b4
Carmine Choi <carmine.choi@gmail.com>
2023-09-27 23:12:03 -0700

    refractor: oauth logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,5 @@
-function authenticate(event) {
-	// Redirect the user to the GitHub OAuth page.
-	const AUTH_URL = "https://github.com/login/oauth/authorize";
-	const CLIENT_ID = "7c89607e07edc93ace0e";
-	const SCOPE = "repo";
-	const url = `${AUTH_URL}?client_id=${CLIENT_ID}&scope=${SCOPE}`;
+import OAuth from "../scripts/oauth.js";
 
-	chrome.tabs.create({ url: url, active: true });
-}
-
-document.getElementById("authenticate").addEventListener("click", authenticate);
+document.getElementById("authenticate").addEventListener("click", (event) => {
+	OAuth.authenticate();
+});
--- scripts/oauth.js
+++ scripts/oauth.js
@@ -1,7 +1,17 @@
-window.onload = function () {
-	document.querySelector("button").addEventListener("click", function () {
-		chrome.identity.getAuthToken({ interactive: true }, function (token) {
-			console.log(token);
-		});
-	});
-};
+export class OAuth {
+	constructor() {
+		this.auth_url = "https://github.com/login/oauth/authorize";
+		this.client_id = "7c89607e07edc93ace0e";
+		// this.redirect_uri = chrome.identity.getRedirectURL("github_oauth");
+		this.scope = "repo";
+	}
+
+	// Redirect the user to the GitHub OAuth page.
+	authenticate() {
+		console.log("authenticate");
+		const url = `${this.auth_url}?client_id=${this.client_id}&scope=${this.scope}`;
+		chrome.tabs.create({ url: url, active: true });
+	}
+}
+
+export default new OAuth();
