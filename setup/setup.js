ed5b11595f23b6b735240012f9be18b0fb5ef14b
Carmine Choi <carmine.choi@gmail.com>
2023-09-30 03:39:52 -0700

    feat: ui supports custom repositories

--- setup/setup.js
+++ setup/setup.js
@@ -1,16 +1,32 @@
 import Github from "../scripts/github.js";
 
+const repoSelect = document.getElementById("repoSelect");
+const repoInput = document.getElementById("repoInput");
+const submitButton = document.getElementById("submitButton");
+
 // Load the users repositories into the options
 document.addEventListener("DOMContentLoaded", function () {
-	const repositorySelect = document.getElementById("repositorySelect");
 	Github.fetchUserRepositories().then((repositories) => {
 		if (Array.isArray(repositories)) {
 			repositories.forEach((repo) => {
 				const option = document.createElement("option");
 				option.value = repo.name;
 				option.textContent = repo.name;
-				repositorySelect.appendChild(option);
+				repoSelect.appendChild(option);
 			});
 		}
 	});
+});
+
+// Add an event listener to the form for submission handling
+document.getElementById("repoSelect").addEventListener("change", function (e) {
+	const selectedOption = repoSelect.value;
+
+	if (selectedOption === "new") {
+		repoInput.style.display = "block";
+		submitButton.textContent = "Create Repository";
+	} else {
+		repoInput.style.display = "none";
+		submitButton.textContent = "Link Repository";
+	}
 });
