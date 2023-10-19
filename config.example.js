061dc467fd7c5867b76c2e37d24365133500199f
Carmine <carmine.choi@gmail.com>
2023-10-18 17:58:35 -0700

    feat: remove unnecessary permission

--- config.example.js
+++ config.example.js
@@ -1,3 +1,5 @@
+// Copy Paste this file and name it config.production.js
+
 export const GITHUB_CLIENT_ID = ""; // Add your GitHub application client ID
 export const GITHUB_CLIENT_SECRET = ""; // Add your GitHub client secret
 export const GITHUB_REDIRECT_URI = ""; // Add your redirect uri
